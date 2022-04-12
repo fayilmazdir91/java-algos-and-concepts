cc2ae453969f70ac8375b88674d59792bc512dac
Fırat <57766894+fayilmazdir91@users.noreply.github.com>
2022-04-12 13:08:59 +0300

    Add files via upload

---
+++ Palindrom.java
@@ -0,0 +1,32 @@
+package patikaJava;
+import java.util.Scanner;
+
+public class Palindrom {
+
+    static boolean isPalindromic(int number){
+
+        int temp = number;
+        int lastNumber,reverse =0;
+
+        while (temp !=0){
+
+            lastNumber = temp % 10;
+            reverse = (reverse * 10) + lastNumber;
+            temp /= 10;
+        }
+           if( number == reverse){
+               System.out.println(number + " is a polindromic number.");
+               return  true;
+           }else{
+               System.out.println(number + " is a not polindromic number.");
+               return false;
+           }
+    }
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int number = inp.nextInt();
+        System.out.println(isPalindromic(number));
+    }
+}
