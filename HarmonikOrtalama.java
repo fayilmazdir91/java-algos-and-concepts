0a121ee6551113a2d95975d82699a8407cd64c92
Fırat <57766894+fayilmazdir91@users.noreply.github.com>
2022-04-12 20:30:51 +0300

    Add files via upload

---
+++ HarmonikOrtalama.java
@@ -0,0 +1,13 @@
+package patikaJava;
+
+public class HarmonikOrtalama {
+	public static void main(String[] args) {
+		int[] numbers = {1, 2, 3, 4, 5};
+		double sum = 0;
+		for(int i = 0; i<numbers.length; i++) {
+			sum += (1/numbers[i]);
+		}
+		
+		System.out.println(sum / numbers.length);
+	}
+}
